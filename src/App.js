c6a06f7b3598abdff122d461550a4913200346ac
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-05-13 00:33:57 +0100

    backend hosted on heroku, added new fetch url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const formHandler = async (e) => {
         e.preventDefault()
 
-        const response = await fetch("https://green-frontend.netlify.app/signup", {
+        const response = await fetch("https://green-signup-backend.herokuapp.com/app/signup", {
             method: "POST",
             headers:{"Content-Type": "application/json"},
             body: JSON.stringify({
