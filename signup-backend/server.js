75c6431efec7b4d2adf3da1d9607b9b1eef5e2bc
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-05-13 00:12:03 +0100

    added port listener for backend

--- signup-backend/server.js
+++ signup-backend/server.js
@@ -7,9 +7,13 @@ const cors = require('cors')
 
 dotenv.config()
 
+const port = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URL, () => console.log("Database connected"))
 
 app.use(express.json())
 app.use(cors())
 app.use('/app', routesUrls)
-app.listen(4000, () => console.log("Server is running"))
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
