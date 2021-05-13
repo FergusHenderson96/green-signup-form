b224596b77b8f54b3d39e8eee7176d9a9a7bf0a7
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-05-13 01:16:13 +0100

    changed to this.state from useState to test fetch error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
 import './app.css';
 
-const App = () => {
+class App extends Component {
 
-    const [email, setEmail] = useState("")
+constructor (){
+    super()
+    this.state = {
+        email: ''
+    }
+    this.changeEmail = this.changeEmail.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+}
 
-
-    const formHandler = async (e) => {
-        e.preventDefault()
-
-        const response = await fetch("https://green-signup-backend.herokuapp.com/signup", {
-            method: "POST",
-            headers:{"Content-Type": "application/json"},
-            body: JSON.stringify({
-                email: email
-            })
-        })
-        const data = await response.json()
-        
+changeEmail(e){
+    this.setState({
+        email:e.target.value
+    })
+}
+onSubmit(e){
+    e.preventDefault()
+    const registered = {
+        email: this.state.email
     }
 
+    axios.post("http://localhost:4000/app/signup", registered)
+    .then(res => console.log(res.data))
+
+    this.setState({
+        email: ''
+    })
+}
+    
+
+    render() {
         return (
             <div>
                 <div className="container">
@@ -34,12 +47,13 @@ const App = () => {
                         <p>Please enter your email below</p>
                         </div>
                         
-                        <form onSubmit={formHandler}>
+                        <form onSubmit={this.onSubmit}>
                             
                             <div className="textBox">
                             <p>Email Address</p>
                             <input type= "email"
-                            onChange={(e) => setEmail(e.target.value)}/>
+                            onChange={this.changeEmail}
+                            value={this.state.email}/>
                             </div>
 
                             <div className="remember">
@@ -57,6 +71,71 @@ const App = () => {
             </div>
         )
     }
-
+}
 
 export default App;
+
+
+
+// import React, { useState } from 'react'
+// import axios from 'axios'
+// import './app.css';
+
+// const App = () => {
+
+//     const [email, setEmail] = useState("")
+
+
+//     const formHandler = async (e) => {
+//         e.preventDefault()
+
+//         const response = await fetch("http://localhost:4000/app/signup", {
+//             method: "POST",
+//             headers:{"Content-Type": "application/json"},
+//             body: JSON.stringify({
+//                 email: email
+//             })
+//         })
+//         const data = await response.json()
+        
+//     }
+
+//         return (
+//             <div>
+//                 <div className="container">
+//                     <div className="img">
+//                     <img src="/images/green.png" alt="logo"/>
+//                     </div> 
+
+//                     <div className="form">
+//                         <div className="title">
+//                         <h1>Welcome to Green.</h1>
+//                         <p>Please enter your email below</p>
+//                         </div>
+                        
+//                         <form onSubmit={formHandler}>
+                            
+//                             <div className="textBox">
+//                             <p>Email Address</p>
+//                             <input type= "email"
+//                             onChange={(e) => setEmail(e.target.value)}/>
+//                             </div>
+
+//                             <div className="remember">
+//                             <input type="checkbox"/>
+//                             <a>Remember this device</a>
+//                             </div>
+                            
+//                             <div className="submit">
+//                             <input type="submit"
+//                             value="Sign In"/>
+//                         </div>    
+//                         </form>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+
+
+// export default App;
