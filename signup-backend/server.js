638b19998244a64a3d4608aa62b1964a317a664b
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-05-13 11:58:56 +0100

    added final working project ready to redeploy

--- signup-backend/server.js
+++ signup-backend/server.js
@@ -4,12 +4,29 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const routesUrls = require('./routes/routes')
 const cors = require('cors')
+const path = require('path')
 
 dotenv.config()
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URL, () => console.log("Database connected"))
+const connection = async () => {
+    //async function which trys to connect to mongoose and will await connection before continuing with function 
+    try {
+      await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+      //use** stops errors
+      console.log("successfully connected to MongoDB");
+      //logs to console if function connects to mongoose
+    } catch (error) {
+      console.log(error);
+      //if theres an error it will log 
+    }
+  };
+  
+  connection();
+
+// mongoose.connect(process.env.MONGO_URL, () => console.log("Database connected"))
+
 
 app.use(express.json())
 app.use(cors())
